1aa781db87edb0b69243f576a82dfe1879590725
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2024-10-08 20:46:35 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const App = () => {
+  return (
+    <div className='container p-4 border mt-2'>
+        <div className='row'>
+            <div className='col-12 text-center'>
+                <p>Hello, lembretes</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
