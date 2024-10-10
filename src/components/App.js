dabb0b431b0b6ee2e82b05db315665b6344fb30d
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2024-10-09 21:33:48 -0300

    feat(p1-pdmt): cria componente de entrada

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LembreteEntrada from './LembreteEntrada'
+
+export default class App extends React.Component {
+
+  state = {
+      lembretes: []
+  }
+
+  onLembreteAdicionado = (lembrete) => {
+    this.state.lembretes.push(lembrete)
+    console.log(this.state.lembretes)
+  }
+
+  render () {
+    return (
+    <div className='container p-4 border mt-2'>
+        <div className='row'>
+            <div className='col-12 text-center'>
+                <LembreteEntrada onLembreteAdicionado={this.onLembreteAdicionado}/>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
+
---
+++ src/components/LembreteEntrada.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class LembreteEntrada extends Component {
+    state = { 
+        lembrete: '' 
+    } 
+    
+    onLembreteEscrito = (event) => { 
+        this.setState({lembrete: event.target.value})
+    } 
+
+    onFormSubmit = (event) => {
+        event.preventDefault()
+        console.log(this.state.lembrete)
+        this.props.onLembreteAdicionado(this.state.lembrete)
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onFormSubmit}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 col-lg-11">
+                            <input 
+                            value={this.state.lembrete} 
+                            type="text" 
+                            class="form-control mb-3" 
+                            onChange={this.onLembreteEscrito} 
+                            placeholder="Digite aqui seu lembrete..."/>
+                        </div>
+                        <div className="col-sm-12 col-lg-1">
+                            <button type="submit" class="btn btn-outline-secondary w-100">Ok</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        )
+    }
+}
